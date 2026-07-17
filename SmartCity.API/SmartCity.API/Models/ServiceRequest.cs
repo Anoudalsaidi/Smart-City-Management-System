e103d711a2ffd6d1e0e7077bc8fe5ab0629dd5ed
Anoudalsaidi <anoudalsaidi1993@gmail.com>
2026-07-17 08:01:03 +0400

    feat: add ServiceRequest entity

---
+++ SmartCity.API/SmartCity.API/Models/ServiceRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartCity.API.Models
+{
+    public class ServiceRequest
+    {
+        [Key]
+        public int ServiceRequestId { get; set; }
+
+        [ForeignKey("Citizen")]
+        public int CitizenId { get; set; }
+
+        public Citizen? Citizen { get; set; }
+
+        [ForeignKey("Service")]
+        public int ServiceId { get; set; }
+
+        public Service? Service { get; set; }
+
+        public DateTime SubmissionDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = "Pending";
+
+        [StringLength(500)]
+        public string? Notes { get; set; }
+    }
+}
--- SmartCity.API/SmartCity.API/obj/Debug/net10.0/SmartCity.API.AssemblyInfo.cs
+++ SmartCity.API/SmartCity.API/obj/Debug/net10.0/SmartCity.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCity.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a5cc9f9dfc50a63417cb43397b1a54f9d1d5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d5efe0d468006b3cb13d68dc32e6d969a73b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCity.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCity.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
