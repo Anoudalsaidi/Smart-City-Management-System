a65cf818eb3a74fd189ef0f548ee16f78b7aa826
Anoudalsaidi <anoudalsaidi1993@gmail.com>
2026-07-17 08:05:27 +0400

    feat: add Notification entity

---
+++ SmartCity.API/SmartCity.API/Models/Notification.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartCity.API.Models
+{
+    public class Notification
+    {
+        [Key]
+        public int NotificationId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(300)]
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime SentAt { get; set; } = DateTime.Now;
+
+        public bool IsRead { get; set; } = false;
+
+        [ForeignKey("Citizen")]
+        public int CitizenId { get; set; }
+
+        public Citizen? Citizen { get; set; }
+    }
+}
